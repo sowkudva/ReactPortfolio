9ffeb9292d8cd770937813ac978317ba5f9eae5d
Sowmya Kudva <skudva@nevada.unr.edu>
2024-08-28 13:54:54 -0700

    prettier update

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import './index.css';
-import {Routes, Route} from 'react-router-dom';
-import Home from './routes/Home'; 
-import  Contact from './routes/Contact'; 
-import  About from './routes/About'; 
-import Projects from './routes/Projects';
+import React from 'react'
+import './index.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './routes/Home'
+import Contact from './routes/Contact'
+import About from './routes/About'
+import Projects from './routes/Projects'
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Home ></Home>}></Route>
-      <Route path="/projects" element={<Projects></Projects>}/>
-      <Route path="/contact" element={<Contact></Contact>}/>
-      <Route path="/about" element={<About></About>}/>
-      <Route path="/experience" element={<About></About>}/>
-      <Route path="/skills" element={<About></About>}/>
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/projects' element={<Projects></Projects>} />
+        <Route path='/contact' element={<Contact></Contact>} />
+        <Route path='/about' element={<About></About>} />
+        <Route path='/experience' element={<About></About>} />
+        <Route path='/skills' element={<About></About>} />
+      </Routes>
     </>
   )
 }
 
-export default App;
+export default App
